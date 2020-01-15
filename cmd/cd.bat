2df46c870eda29e295a0ab1b11b5ae348358621b
Tzu-ping Chung <uranusjr@gmail.com>
2020-01-15 23:47:34 +0800

    Implement cd -

---
+++ cmd/cd.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM This does not directly work in normal cmd.exe because "cd" is a built-in.
+REM You need to register this as a macro with DOSKEY.
+
+if not "%1" == "-" (
+    cd %1
+    set OLDPWD=%CD%
+) else if "%OLDPWD%" == "" (
+    echo cd: %%OLDPWD%% not set
+    exit /b 1
+) else (
+    set OLDPWD=%CD%
+    cd %OLDPWD%
+)
