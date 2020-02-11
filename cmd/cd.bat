bfdd58bf17f6b7c579cd7ccd04a2af39f6a00880
Tzu-ping Chung <uranusjr@gmail.com>
2020-02-12 03:15:47 +0800

    Fix cd to support cross-disk and spaces in path

--- cmd/cd.bat
+++ cmd/cd.bat
@@ -3,13 +3,13 @@
 REM This does not directly work in normal cmd.exe because "cd" is a built-in.
 REM You need to register this as a macro with DOSKEY.
 
-if not "%1" == "-" (
-    cd %1
+if not "%~1" == "-" (
+    cd /d %1
     set OLDPWD=%CD%
 ) else if "%OLDPWD%" == "" (
     echo cd: %%OLDPWD%% not set
     exit /b 1
 ) else (
     set OLDPWD=%CD%
-    cd %OLDPWD%
+    cd /d %OLDPWD%
 )
